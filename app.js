bffb92ccf100298404e45f450fbc97a19f900e9f
Stephen <sagelvl40@gmail.com>
2023-09-19 15:54:58 +0100

    finalise questions code and add the score counter

--- app.js
+++ app.js
@@ -11,53 +11,64 @@ function kenobi() {
 }
 
 function steveQuiz() {
+  let correctAnswers = 0;
   let q1 = prompt("Is Steve 41?");
 
-  if (q1.toLowerCase() === "yes" || "y") {
-    console.log("Yahtzee!");
+  if (q1.toLowerCase() === "yes" || q1.toLowerCase() === "y") {
+    correctAnswers++;
+    // console.log("Yahtzee!");
     alert("Yahtzee!");
   } else {
-    console.log("Wrong!");
+    // console.log("Wrong!");
     alert("Wrong!");
   }
 
   let q2 = prompt("Steve's Wife is called Kelly?");
 
-  if (q2.toLowerCase() === "yes" || "y") {
-    console.log("Yahtzee!");
+  if (q2.toLowerCase() === "yes" || q2.toLowerCase() === "y") {
+    correctAnswers++;
+    // console.log("Yahtzee!");
     alert("Yahtzee!");
   } else {
-    console.log("Wrong!");
+    // console.log("Wrong!");
     alert("Wrong!");
   }
 
   let q3 = prompt("Steve supports Arsenal?");
 
-  if (q3.toLowerCase() === "no" || "n") {
+  if (q3.toLowerCase() === "no" || q3.toLowerCase() === "n") {
+    // correctAnswers++;
     console.log("Yahtzee!");
     alert("Yahtzee!");
   } else {
-    console.log("Wrong!");
+    // console.log("Wrong!");
     alert("Wrong!");
   }
 
   let q4 = prompt("Steve Bloody Loves Star Wars");
 
-  if (q4.toLowerCase() === "yes" || "n") {
-    console.log("Yahtzee!");
+  if (q4.toLowerCase() === "yes" || q4.toLowerCase() === "y") {
+    correctAnswers++;
+    // console.log("Yahtzee!");
     alert("Yahtzee!");
   } else {
-    console.log("Wrong!");
+    // console.log("Wrong!");
     alert("Wrong!");
   }
 
   let q5 = prompt("Kofi is named after an astronaught?");
 
-  if (q5.toLowerCase() === "no" || "n") {
-    console.log("Yahtzee!");
+  if (q5.toLowerCase() === "no" || q5.toLowerCase() === "n") {
+    correctAnswers++;
+    // console.log("Yahtzee!");
     alert("Yahtzee!");
   } else {
-    console.log("Wrong!");
+    // console.log("Wrong!");
     alert("Wrong!");
   }
+  alert(
+    username +
+      " Well done" +
+      ` you got ${correctAnswers} out of 5 correct answers!`
+  );
 }
