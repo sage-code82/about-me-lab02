765a4fb09958d783cd75d222a0b7b8c00c141d11
Stephen <sagelvl40@gmail.com>
2023-09-19 14:25:00 +0100

    add questions into button with alerts

--- app.js
+++ app.js
@@ -9,44 +9,55 @@ function kenobi() {
   welcome = "Hello " + username + " Welcome to the Book of Steve!";
   document.write(welcome);
 }
+
 function steveQuiz() {
-  let q1 = Prompt("Is Steve 41?");
+  let q1 = prompt("Is Steve 41?");
 
-  if (q1 === true) {
-    console.log("Yes!");
+  if (q1.toLowerCase() === "yes" || "y") {
+    console.log("Yahtzee!");
+    alert("Yahtzee!");
   } else {
     console.log("Wrong!");
+    alert("Wrong!");
   }
 
-  let q2 = Prompt("Steve's Wife is called Kelly?");
+  let q2 = prompt("Steve's Wife is called Kelly?");
 
-  if (q2 === true) {
-    console.log("Yes!");
+  if (q2.toLowerCase() === "yes" || "y") {
+    console.log("Yahtzee!");
+    alert("Yahtzee!");
   } else {
     console.log("Wrong!");
+    alert("Wrong!");
   }
 
-  let q3 = Prompt("Steve supports Arsenal?");
+  let q3 = prompt("Steve supports Arsenal?");
 
-  if (q3 === true) {
-    console.log("Yes!");
+  if (q3.toLowerCase() === "no" || "n") {
+    console.log("Yahtzee!");
+    alert("Yahtzee!");
   } else {
     console.log("Wrong!");
+    alert("Wrong!");
   }
 
-  let q4 = Prompt("Steve Bloody Loves Star Wars");
+  let q4 = prompt("Steve Bloody Loves Star Wars");
 
-  if (q4 === true) {
-    console.log("Yes!");
+  if (q4.toLowerCase() === "yes" || "n") {
+    console.log("Yahtzee!");
+    alert("Yahtzee!");
   } else {
     console.log("Wrong!");
+    alert("Wrong!");
   }
 
-  let q5 = Prompt("Kofi is named after an astronaught?");
+  let q5 = prompt("Kofi is named after an astronaught?");
 
-  if (q5 === true) {
-    console.log("Yes!");
+  if (q5.toLowerCase() === "no" || "n") {
+    console.log("Yahtzee!");
+    alert("Yahtzee!");
   } else {
     console.log("Wrong!");
+    alert("Wrong!");
   }
 }
