65cee9df4dc96c253eb1a473982f5ba3fda87954
Stephen <sagelvl40@gmail.com>
2023-09-20 22:55:24 +0100

    everything forgot to commit today

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ Plan for today in this order! :
 
 ### Tasks
 
-- update my work from yesterday with the highlighted items from this mornings recap
-- include in my html 2 lists a top 10 list and an unordered list about my work history
-- Add a 6th question to the guessing game, that takes a numeric display with a too high or to low alert with 4 attempts, and then telling the user the correct answer.
-- Add a 7th question that has multiple correct answers using an array, give them 6 attempts to get the correct answer, then display all possible correct answers,
+- update my work from yesterday with the highlighted items from this mornings recap **done had lots of problems! took too long!!!**
+- include in my html 2 lists a top 10 list and an unordered list about my work history **done**
+- Add a 6th question to the guessing game, that takes a numeric display with a too high or to low alert with 4 attempts, and then telling the user the correct answer. **done**
+- Add a 7th question that has multiple correct answers using an array, give them 6 attempts to get the correct answer, then display all possible correct answers, **done** _however could not get the code to work to if they input a letter_
 - Keep track of the score **I have all ready done this!**
 
-### Stretch Goals
+### Stretch Goals _unfortunatley did not get a chance to touch these_
 
 - Attempt to make your code more DRY by putting all of the questions, answers, and responses to the first five yes/no questions into arrays (or even one huge multidimensional array), and modifying the game logic such that a ‘for’ loop will control the flow from question to question.
 - Make the number-guessing question use a random number as its correct answer.
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log("Hello There!");
 ("use strict");
 
 let username = prompt("Hello There! What is your name?");
-console.log(username); // add a while loop to here to stop them from pressing cancel 20.03.23
+console.log(username);
 
 let welcome;
 
@@ -11,68 +11,135 @@ function kenobi() {
   welcome = "Hello " + username + " Welcome to the Book of Steve!";
   document.write(welcome);
 }
-// missing while loop this will force the user to say Yes or No from 20.0.23 recap
+// missing while loop this will force the user to say Yes or No from 20.0.23 recap CGPT
 
 function steveQuiz() {
   let correctAnswers = 0;
-  let q1 = prompt("Is Steve 41?"); // toLowerCase can also go here change this as over complicating my code
 
-  if (q1.toLowerCase() === "yes" || q1.toLowerCase() === "y") {
-    correctAnswers++;
-    // console.log("Yahtzee!");
-    alert("Yahtzee!");
-  } else {
-    // console.log("Wrong!");
-    alert("Wrong!");
+  let yayOrNay = false;
+  while (!yayOrNay) {
+    let q1 = prompt("Is Steve 41?").toLowerCase();
+
+    if (q1 === "yes" || q1 === "y") {
+      correctAnswers++;
+      alert("Yahtzee!");
+      yayOrNay = true;
+    } else if (q1 === "no" || q1 === "n") {
+      alert("Wrong!");
+      yayOrNay = true;
+    } else {
+      alert("This is not the way. Please enter 'yes' or 'no'.");
+    }
+  }
+  yayOrNay = false;
+  while (!yayOrNay) {
+    let q2 = prompt("Steve's Wife is called Kelly?").toLowerCase();
+
+    if (q2 === "yes" || q2 === "y") {
+      correctAnswers++;
+      alert("Yahtzee!");
+      yayOrNay = true;
+    } else if (q2 === "no" || q2 === "n") {
+      alert("Wrong!");
+      yayOrNay = true;
+    } else {
+      alert("This is not the way. Please enter 'yes' or 'no'.");
+    }
+  }
+  yayOrNay = false;
+  while (!yayOrNay) {
+    let q3 = prompt("Steve supports Arsenal?").toLowerCase();
+    if (q3 === "no" || q3 === "n") {
+      correctAnswers++;
+      alert("Yahtzee!");
+      yayOrNay = true;
+    } else if (q3 === "yes" || q3 === "y") {
+      alert("Wrong!");
+      yayOrNay = true;
+    } else {
+      alert("This is not the way. Please enter 'yes' or 'no'.");
+    }
   }
 
-  let q2 = prompt("Steve's Wife is called Kelly?");
-
-  if (q2.toLowerCase() === "yes" || q2.toLowerCase() === "y") {
-    correctAnswers++;
-    // console.log("Yahtzee!");
-    alert("Yahtzee!");
-  } else {
-    // console.log("Wrong!");
-    alert("Wrong!");
+  yayOrNay = false;
+  while (!yayOrNay) {
+    let q4 = prompt("Steve Bloody Loves Star Wars").toLowerCase();
+    if (q4 === "yes" || q4 === "y") {
+      correctAnswers++;
+      alert("Yahtzee!");
+      yayOrNay = true;
+    } else if (q4 === "no" || q4 === "n") {
+      alert("Wrong!");
+      yayOrNay = true;
+    } else {
+      alert("This is not the way. Please enter 'yes' or 'no'.");
+    }
   }
 
-  let q3 = prompt("Steve supports Arsenal?");
+  yayOrNay = false;
+  while (!yayOrNay) {
+    let q5 = prompt("Kofi is named after an astronaught?").toLowerCase();
 
-  if (q3.toLowerCase() === "no" || q3.toLowerCase() === "n") {
-    correctAnswers++;
-    // console.log("Yahtzee!");
-    alert("Yahtzee!");
-  } else {
-    // console.log("Wrong!");
-    alert("Wrong!");
+    if (q5 === "no" || q5 === "n") {
+      correctAnswers++;
+      alert("Yahtzee!");
+      yayOrNay = true;
+    } else if (q5 === "yes" || q5 === "y") {
+      alert("Wrong!");
+      yayOrNay = true;
+    } else {
+      alert("This is not the way. Please enter 'yes' or 'no'.");
+    }
   }
 
-  let q4 = prompt("Steve Bloody Loves Star Wars");
+  let kofiAge = parseInt(prompt("Take a guess... How old is my son Kofi?"));
+  let userAttempts = 0;
 
-  if (q4.toLowerCase() === "yes" || q4.toLowerCase() === "y") {
-    correctAnswers++;
-    // console.log("Yahtzee!");
-    alert("Yahtzee!");
-  } else {
-    // console.log("Wrong!");
-    alert("Wrong!");
+  for (let i = 0; i < 3; i++) {
+    userAttempts++;
+
+    if (kofiAge > 3) {
+      kofiAge = parseInt(prompt("Not quite, my Padawan is a bit younger")); //not sure if this is entirely right
+    } else if (kofiAge < 3) {
+      kofiAge = parseInt(prompt("MOOOORRRRREEEE"));
+    } else {
+      correctAnswers++;
+      alert("Good Gooooood ");
+      break;
+    }
   }
 
-  let q5 = prompt("Kofi is named after an astronaught?");
+  if (userAttempts === 3 && kofiAge !== 2) {
+    alert("You have failed me for the last time! The correct answer is 3.");
+  }
 
-  if (q5.toLowerCase() === "no" || q5.toLowerCase() === "n") {
-    correctAnswers++;
-    // console.log("Yahtzee!");
-    alert("Yahtzee!");
-  } else {
-    // console.log("Wrong!");
-    alert("Wrong!");
+  let wineVisit = ["chile", "usa", "portugal", "france"];
+
+  let wineQuestion = prompt(
+    "While working in the Wine Industry I got to travel a abit, try and take a guess at any of the places I visited."
+  ).toLowerCase();
+  let correctAnswer = 0;
+
+  for (let i = 0; i < 5; i++) {
+    if (wineVisit.includes(wineQuestion)) {
+      correctAnswer++;
+      alert("Noice! Good job");
+      break;
+    } else {
+      wineQuestion = prompt("Never heard of it").toLowerCase();
+    }
+  }
+
+  if (correctAnswer === 0) {
+    alert(
+      "You have failed me for the last time! The correct answers were: " +
+        wineVisit.join(", ")
+    );
   }
 
   alert(
     username +
       " Well done" +
-      ` you got ${correctAnswers} out of 5 correct answers!`
+      ` you got ${correctAnswers} out of 7 correct answers!`
   );
 }
